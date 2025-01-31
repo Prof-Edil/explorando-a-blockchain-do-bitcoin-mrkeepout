c689af1b3d2c159b99df846e961c7c308c50ddaa
Gabriel Nascimento <190107111@aluno.unb.br>
2025-01-30 23:06:27 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,12 +1,9 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 #!/bin/bash
-# Descritor 
-descriptor="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100/*)"
 
-# Obtaining descriptor checksum
-descriptor_info=$(bitcoin-cli getdescriptorinfo "$descriptor")
-corrected_descriptor=$(echo $descriptor_info | jq -r '.descriptor')
+# Generate a valid descriptor with a checksum
+descriptor=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)" | jq -r .descriptor)
 
-# Deriva o endereço Taproot no índice 100 usando o descritor corrigido
-bitcoin-cli deriveaddresses "$corrected_descriptor" "[100,100]"
+# Taproot at 100
+bitcoin-cli deriveaddresses "$descriptor" "[100,100]" | jq -r '.[0]'
