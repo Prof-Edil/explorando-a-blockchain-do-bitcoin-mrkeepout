95326ee8beb1fcb5d81e8b3f8ac4c3eb04fe83c2
Gabriel Nascimento <190107111@aluno.unb.br>
2025-01-30 23:23:23 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -5,4 +5,4 @@ txid="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
 tx=$(bitcoin-cli getrawtransaction $txid 1)
 keys=$(echo $tx | jq -r '.vin[].txinwitness | select(.) | .[1]')
 keys_array=$(echo "$keys" | jq -s -R 'split("\n")[:-1]')
-bitcoin-cli createmultisig 1 "$keys_array"
+bitcoin-cli createmultisig 1 "$keys_array" | jq -r '.address'
