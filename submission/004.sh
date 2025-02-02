7597e5726c1a342f9750fb685a679612ff944f42
Gabriel Nascimento <190107111@aluno.unb.br>
2025-02-02 15:01:40 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -2,8 +2,6 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 #!/bin/bash
 
-# Generate a valid descriptor with a checksum
-descriptor=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)" | jq -r .descriptor)
+descriptor=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)" | jq -r .descriptor)
 
-# Taproot at 100
 bitcoin-cli deriveaddresses "$descriptor" "[100,100]" | jq -r '.[0]'
